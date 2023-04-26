3be98e09fd2617c76b4e118d6c1e7d35254434c7
mmindd <supaphit.mind@gmail.com>
2023-04-26 14:38:36 +0700

    mmindd

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class program{
+    static void Main (string[] args){
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        
+
+        while(x<1){
+            Console.WriteLine("end");
+            break;
+        }
+        if(x>1)
+        {
+            int[] num = new int[2];
+       for (int i = 1; i < 7; ++i)
+       {
+        num[i] = int.Parse(Console.ReadLine());
+       }
+       while(true){
+        Console.Write("Input number: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number <= 0 || number > 7)
+        {
+            break;
+        }
+        Console.WriteLine("{0}", num[number - 1]);
+       }
+        Console.WriteLine("end");
+        }
+       
+        
+    }
+}
